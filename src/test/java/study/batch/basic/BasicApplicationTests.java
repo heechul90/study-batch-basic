9f588a4be74f66911b478dfaf7b434980c155be6
hclee <heechul4296@gmail.com>
2022-10-16 23:17:46 +0900

    spring batch source upload

---
+++ src/test/java/study/batch/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package study.batch.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
